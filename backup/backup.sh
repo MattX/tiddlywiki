8ed97f790ac58e27b3fb1d0f8c1a382ce15609f9
Matthieu Felix <matthieufelix@gmail.com>
2021-07-05 15:05:37 -0400

    Fix a few glitches

--- backup/backup.sh
+++ backup/backup.sh
@@ -5,8 +5,8 @@ git add .
 
 git diff --cached --exit-code || (echo "No changes detected, exiting"; exit 0)
 
-git commit -m "Automatic commit at $(date --rfc-3339 seconds --utc)"
+git commit -m "Automatic commit at $(date -Iseconds --utc)"
 git config user.name "Matthieu Felix"
 git config user.email "matthieufelix@gmail.com"
-git remote set-url origin "https://MattX:$(cat /etc/github-api-key.txt)@github.com/MattX/wiki"
+git remote set-url origin "https://MattX:$(cat /etc/github-api-key.txt)@github.com/MattX/zettelkasten"
 git push
